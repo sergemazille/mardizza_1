2fbcef2966bce9ec25f333b0f49f64d279e598e4
Serge <serge.mazille@gmail.com>
2016-08-28 21:25:39 +0200

    Modified: style assets with gulp (sass and concat)

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,30 @@
 'use strict';
 
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 
+// Scripts
 gulp.task('js', function () {
     return gulp.src('./src/AppBundle/Resources/public/js/**/*.js')
         .pipe(gulp.dest('./web/assets/js'));
 
 });
 
+// Style
 gulp.task('sass', function () {
-    return gulp.src('./src/AppBundle/Resources/public/sass/**/*.scss')
-        .pipe(gulp.dest('./web/assets/css'));
-
+    return gulp.src('./src/AppBundle/Resources/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./src/AppBundle/Resources/public/css/'));
 });
 
+gulp.task('concat-css', ['sass'], function (){
+    return gulp.src('./src/AppBundle/Resources/public/css/*.css')
+        .pipe(concat('style.css'))
+        .pipe(gulp.dest('./web/assets/css'))
+});
+
+// Images
 gulp.task('images', function () {
     return gulp.src('./src/AppBundle/Resources/public/images/**/*.jpg')
         .pipe(gulp.dest('./web/assets/images'));
@@ -23,11 +34,11 @@ gulp.task('images', function () {
 // Watches
 gulp.task('watch', function () {
     gulp.watch('./src/AppBundle/Resources/js/**/*.js', ['js']);
-    gulp.watch('./src/AppBundle/Resources/sass/**/*.scss', ['sass']);
+    gulp.watch('./src/AppBundle/Resources/**/*.scss', ['sass', 'concat-css']);
 });
 
 // Default
-gulp.task('default', ['js', 'sass', 'images', 'watch']);
+gulp.task('default', ['js', 'sass', 'concat-css', 'images', 'watch']);
 
 // Deploy only (without watch task)
-gulp.task('deploy', ['js', 'sass', 'images']);
+gulp.task('deploy', ['js', 'sass', 'concat-css', 'images']);
