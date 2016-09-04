89cc53a8b5780d62edb6e6c3e731f47745fcb1b6
Serge <serge.mazille@gmail.com>
2016-09-04 09:58:22 +0200

    Created login route, controller, view

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class SecurityController extends Controller
+{
+    public function loginAction()
+    {
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            '@App/login.html.twig', [
+                'last_username' => $lastUsername,
+                'error' => $error,
+            ]
+        );
+    }
+}
