f1fa3df728fe470301026b8f5c4fed4292253bcd
Serge Mazille <serge@wizacha.com>
2016-09-07 13:35:18 +0200

    Order - get or create

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class PageController extends Controller
@@ -21,11 +22,29 @@ class PageController extends Controller
             return $this->redirectToRoute('login');
         }
 
+        $em = $this->getDoctrine()->getManager();
+        $orderRepository = $em->getRepository('AppBundle:Order');
+        $today = new DateTime('TODAY');
+
+        // check if there's already an order today
+        $todaysOrder = $orderRepository->findOneBy(['createdAt' => $today]);
+
+        // else create a new one
+        if(! $todaysOrder){
+            $todaysOrder = $this->get('mardizza.order');
+            $todaysOrder->setCreatedAt($today);
+            $todaysOrder->setUpdatedAt($today);
+
+            $em->persist($todaysOrder);
+            $em->flush();
+        }
+
         $pizzas = $this->get('mardizza.pizza')->getPizzas();
 
         return $this->render('@App/order.html.twig', [
             'pizzas' => $pizzas,
             'user' => $user,
+            'order' => $todaysOrder,
         ]);
     }
 }
