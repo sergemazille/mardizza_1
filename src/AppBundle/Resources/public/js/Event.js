fb43b7d7e60bfe2c81cc3f5429708e196de2af51
Serge <serge.mazille@gmail.com>
2016-09-14 21:55:25 +0200

    fix bug

--- src/AppBundle/Resources/public/js/Event.js
+++ src/AppBundle/Resources/public/js/Event.js
@@ -111,7 +111,7 @@ export class Event {
 
             Helper.switchFavoritesClasses(e.currentTarget);
 
-            let pizzaId = $(this).closest(".pizza-card").data("pizzaId");
+            let pizzaId = $(this).closest(".pizza-card").data("pizzaid");
             Helper.addPizzaToFavorites(pizzaId, function () {
             });
         });
@@ -122,7 +122,7 @@ export class Event {
             
             Helper.switchFavoritesClasses(e.currentTarget);
 
-            let pizzaId = $(this).closest(".pizza-card").data("pizzaId");
+            let pizzaId = $(this).closest(".pizza-card").data("pizzaid");
             Helper.removePizzaFromFavorites(pizzaId, function () {
             });
         });
@@ -131,13 +131,13 @@ export class Event {
 
     // Pizza removal behaviour
     static removePizza(pizzaId) {
-        let $pizzaRow = Dom.getSelectedPizzaRow(pizzaid);
+        let $pizzaRow = Dom.getSelectedPizzaRow(pizzaId);
 
         // remove from database
         $pizzaRow.find('.pizza-remove').on('click', function () {
 
             // check if current user is owner
-            let pizzaOwner = Dom.getPizzaOwnerUsername(pizzaid);
+            let pizzaOwner = Dom.getPizzaOwnerUsername(pizzaId);
 
             if (Helper.isOwner(pizzaOwner)) {
                 let pizzaReference = FirebaseDb.getPizzaReference(Dom.getOrderReference(), pizzaId);
