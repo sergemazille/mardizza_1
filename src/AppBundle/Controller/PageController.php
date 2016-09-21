13d14b016fe12f0f7647895346c2528ccbeeb9fb
Serge <serge.mazille@gmail.com>
2016-09-21 21:01:14 +0200

    Vuejs setup

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -49,7 +49,7 @@ class PageController extends Controller
         $this->addFlash("success", "Bon appétit !!!");
 
         // get pizzas
-        $pizzas = $this->get('mardizza.pizza_service')->getPizzas();
+        $pizzas = $this->get('mardizza.pizza_service')->getJsonPizzas();
 
         // get favorite pizzas
         $favorites = [];
--- src/AppBundle/Service/PizzaService.php
+++ src/AppBundle/Service/PizzaService.php
@@ -3,6 +3,9 @@
 namespace AppBundle\Service;
 
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class PizzaService
 {
@@ -14,5 +17,21 @@ class PizzaService
     public function getPizzas()
     {
         return $this->em->getRepository('AppBundle:Pizza')->findAll();
+    }
+
+    public function getJsonPizzas()
+    {
+        $normalizer = array(new ObjectNormalizer());
+        $encoder = array(new JsonEncoder());
+        $serializer = new Serializer($normalizer, $encoder);
+
+        $pizzas = $this->em->getRepository('AppBundle:Pizza')->findAll();
+
+        $pizzasTab = [];
+        foreach($pizzas as $pizza){
+            $pizzasTab[] = $serializer->serialize($pizza, 'json');
+        }
+
+        return $serializer->serialize($pizzas, 'json');
     }
 }
