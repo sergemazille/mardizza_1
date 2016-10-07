2c4b99577a9d350c99670345afb3a5edb5e9579e
Serge <serge.mazille@gmail.com>
2016-10-07 22:31:06 +0200

    cleanup

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -2,7 +2,6 @@
 
 namespace AppBundle\Entity;
 
-use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\ORM\PersistentCollection;
 use Symfony\Component\Security\Core\User\UserInterface;
