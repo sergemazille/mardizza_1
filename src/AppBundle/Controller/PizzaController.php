85ff34fa4221fc23294959f01a4a370f5213a1cc
Serge <serge.mazille@gmail.com>
2016-10-29 19:47:00 +0200

    Cleanup

--- src/AppBundle/Controller/PizzaController.php
+++ src/AppBundle/Controller/PizzaController.php
@@ -5,7 +5,6 @@ namespace AppBundle\Controller;
 
 use AppBundle\Entity\Pizza;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class PizzaController extends Controller
 {
